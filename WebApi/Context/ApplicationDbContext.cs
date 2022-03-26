b8a3e2590a80e6964bcba9fea52be314311c5407
Richard Oswaldo Negrón Gonzáles <richard.negron.257@gmail.com>
2022-03-26 13:38:01 -0500

    Add project files.

---
+++ WebApi/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+namespace WebApi.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
